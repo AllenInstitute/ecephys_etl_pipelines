9f8f1b705fdd9d9e48489c58d53c4d3be210e2f9
Nicholas Mei <nicholas.mei@alleninstitute.org>
2021-05-17 16:01:07 -0700

    Update setup.py and requirements for this repository

--- setup.py
+++ setup.py
@@ -1,19 +1,26 @@
 from setuptools import setup, find_packages
 
 with open("requirements.txt", "r") as f:
-    required = f.read().splitlines()
+    requirements = f.read().splitlines()
+
+with open("test_requirements.txt", "r") as f:
+    test_requirements = f.read().splitlines()
 
 setup(
-      name="ecephys_etl_pipelines",
-      use_scm_version=True,
-      description=("Pipelines and transforms for processing extracellular "
-                   "electrophysiology data."),
-      author=("Dan Kapner, Nicholas Mei, Scott Daniel, "
-              "Adam Amster, Taylor Matyasz"),
-      author_email="nicholas.mei@alleninstitute.org",
-      url="https://github.com/AllenInstitute/ecephys_etl_pipelines",
-      package_dir={"": "src"},
-      packages=find_packages(where="src"),
-      setup_requires=["setuptools_scm"],
-      install_requires=required,
+    name="ecephys_etl_pipelines",
+    use_scm_version=True,
+    description=("Pipelines and transforms for processing extracellular "
+                "electrophysiology data."),
+    author=("Dan Kapner, Nicholas Mei, Scott Daniel, "
+            "Adam Amster, Taylor Matyasz"),
+    author_email="nicholas.mei@alleninstitute.org",
+    url="https://github.com/AllenInstitute/ecephys_etl_pipelines",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.8",
+    setup_requires=["setuptools_scm"],
+    install_requires=requirements,
+    extras_requires={
+        "test": test_requirements
+    }
 )
